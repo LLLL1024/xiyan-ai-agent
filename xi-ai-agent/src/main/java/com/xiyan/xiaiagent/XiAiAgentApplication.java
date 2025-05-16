142dc4770a6af21336f9042437972a0c182bd736
LLLL1024 <1611857077@qq.com>
2025-05-16 22:44:57 +0800

    后端初始化、整合 Hutool 和 Knife4j 依赖、整合 Spring AI、LangChain4j 等框架，实现 4 种程序调用 AI 大模型的示例代码

---
+++ xi-ai-agent/src/main/java/com/xiyan/xiaiagent/XiAiAgentApplication.java
@@ -0,0 +1,13 @@
+package com.xiyan.xiaiagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XiAiAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XiAiAgentApplication.class, args);
+    }
+
+}
---
+++ xi-ai-agent/src/test/java/com/xiyan/xiaiagent/XiAiAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiyan.xiaiagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XiAiAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
