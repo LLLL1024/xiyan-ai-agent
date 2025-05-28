bb4c250bfd9c2e332467f4a080792ac5206b33cf
LLLL1024 <1611857077@qq.com>
2025-05-28 21:15:30 +0800

    恋爱大师应用整合高德地图 MCP 服务且能够推荐约会地点、开发图片搜索 MCP Server

---
+++ xi-ai-agent/xi-image-search-mcp-server/src/test/java/com/xiyan/xiimagesearchmcpserver/XiImageSearchMcpServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiyan.xiimagesearchmcpserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XiImageSearchMcpServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
